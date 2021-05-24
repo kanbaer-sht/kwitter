7353602685737dd1f9b53b095bb1d85a1536cbd2
kanbaer-sht <kanbaer.sht@gmail.com>
2021-05-24 13:12:56 +0900

    add Kweet and firestore connection::2021-05-24

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { dbService } from '../fbConf';
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+    const [kweet, setKweet] = useState("");
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        await dbService.collection("kweet").add({
+            kweet,
+            createdAt: Date.now()
+        });
+        setKweet("");
+    }
+
+    const onChange = (e) => {
+        setKweet(e.target.value);
+    }
+
+    return (
+        <>
+        <div>
+            <form onSubmit={onSubmit}>
+                <input 
+                value={kweet}
+                onChange={onChange}
+                type="text" 
+                placeholder="What's on your mind?" 
+                maxLength={120} 
+                />
+                <input 
+                type="submit" 
+                value="Kweet" 
+                />
+            </form>
+        </div>
+        </>
+    );
+}
 
 export default Home;
