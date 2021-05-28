8d24e8bc9eecbb0f3597971d0500672937ff54d8
kanbaer-sht <kanbaer.sht@gmail.com>
2021-05-28 11:39:11 +0900

    fixed google login error::2021-05-28

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navigation = ({ userObj }) => {
+    console.log(userObj);
     return (
         <nav>
             <ul>
@@ -9,11 +10,11 @@ const Navigation = ({ userObj }) => {
                     <Link to="/">Home</Link>
                 </li>
                 <li>
-                <Link to="/profile">{userObj.displayName}'s Profile</Link>
+                <Link to="/profile">{userObj?`${userObj.displayName}'s Profile`:"Profile"}</Link>
                 </li>
             </ul>
         </nav>
     );
 }
 
-export default Navigation;
+export default Navigation;            
